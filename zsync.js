a521f765f18b278640bd9e8081707030f35d9b7a
delemercier <delemercier@gmail.com>
2022-10-15 13:19:27 +0200

    update

--- zsync.js
+++ zsync.js
@@ -2,7 +2,7 @@ import { FilesManager } from 'turbodepot-node';
 import fs from 'fs';
 
 const way = process.argv[2] || 'leaderToApps';
-const waiting = 5000
+const waiting = 5
 
 let filesManager = new FilesManager();
 
@@ -12,33 +12,35 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const repos = fs.readdirSync(`..`).filter(dir => dir !== 'leader');
 const paths = ['src/routes/-', 'src/lib/-', 'tests/-', 'static/-']
 
+function printProgress(progress) {
+    process.stdout.write(progress +'\r');
+}
 
 async function main() {
     while (true) {
-        console.log('sync directories...')
         const appsByRepo = {};
         repos.forEach(repo => appsByRepo[repo] = fs.readdirSync(`../${repo}`));
 
-        for(const repo in appsByRepo){
+        for (const repo in appsByRepo) {
             const apps = appsByRepo[repo];
-          
+
             apps.forEach(app => {
                 paths.forEach(p => {
-            
+
                     const source = way === 'leaderToApps' ? `${p}/${app}` : `../${repo}/${app}/${p}`;
                     const target = way === 'leaderToApps' ? `../${repo}/${app}/${p}` : `${p}/${app}`;
 
                     if (fs.existsSync(source)) {
                         if (!fs.existsSync(target)) fs.mkdirSync(target, { recursive: true })
-                        console.log('mirror', source, 'into', target)
+                        printProgress(`${new Date().toLocaleString()} - Syncing every ${waiting} seconds`)
                         filesManager.mirrorDirectory(source, target)
                     }
                 })
             })
         }
-    
+
         if (way !== 'leaderToApps') break;
-        await sleep(waiting);
+        await sleep(waiting * 1000);
     }
 }
 
